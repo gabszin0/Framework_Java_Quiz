07a32b4ce7f37d73eda20d4811b07aacbdfa4246
Gabriel Almeida <125758689+gabszin0@users.noreply.github.com>
2026-07-22 13:38:31 -0300

    Update Main.java

--- aplicacao1/cliente/Main.java
+++ aplicacao1/cliente/Main.java
@@ -6,11 +6,12 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> alternativas = new ArrayList<>();
-        alternativas.add("1");
-        alternativas.add("2");
-        alternativas.add("3");
-        alternativas.add("4");
-        Pergunta p = new Pergunta("Quanto é 1+1",alternativas,2); // a resposta correta aqui é a posição da alternativa
-        new TelaQuizSwing(p);                                                                // se é a primeira,segunda ou quarta alternativa
+        alternativas.add("imprimeF();");
+        alternativas.add("postf();");
+        alternativas.add("printf();");
+        alternativas.add("System.out.println()");
+        Pergunta p = new Pergunta("Qual é o comando para imprimir dados em C? ",alternativas,3); // a resposta correta aqui é a posição da alternativa
+        new TelaQuizSwing(p);                                                                                               // se é a primeira,segunda ou quarta alternativa
     }
+    
 }
